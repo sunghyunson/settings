088058fd7b75af83a77adf0f54061a4b132b4755
son <sh.son@lgepartner.com>
2019-03-26 16:50:34 +0900

    build.sh chagne

--- vscode/cplus/build.sh
+++ vscode/cplus/build.sh
@@ -3,19 +3,30 @@
 makeFileName="Makefile"
 PWD=$1
 cd "$PWD"
-depth=0
+# depth=0
+root="/"
+found="true"
 
 while [[ ! -f "$makeFileName" ]]
 do
+	echo "PWD : $PWD"
 	cd ..
-	depth=$depth+1
-	if [[ $depth -gt 7 ]]
-	then break
+	# depth=$depth+1
+	# if (( $depth > 10 ))
+	# then
+	# 	found="false"
+	# 	break
+	# fi
+	if [[ "$PWD" = "$root" ]]
+	then
+		found="false"
+		break
 	fi
 done
 
-if [[ $depth -gt 7 ]]; then
+if [[ $found = "false" ]]; then
 	echo "ERROR --> no make file"
+	exit 1
 else
 	make $2
 fi
